e1e3c62e146a67097c27d0a31355774ceb6a1540
Scott Helm <helm.scott@gmail.com>
2016-01-18 08:34:54 -0500

    rubocop cleanup

--- lib/streamer.rb
+++ lib/streamer.rb
@@ -7,5 +7,4 @@ require 'streamer/fact_providers'
 # Streamer is used to stream data through a configurable set of functions.
 # The Stream is configurable with a StreamBuilder
 module Streamer
-
 end
--- lib/streamer/version.rb
+++ lib/streamer/version.rb
@@ -1,3 +1,4 @@
+# Streamer version
 module Streamer
   VERSION = '0.1.0'
 end
