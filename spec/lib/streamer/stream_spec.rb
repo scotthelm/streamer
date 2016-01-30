9d347fd9945af481ed2776b5da2f7f17df20300d
Scott Helm <helm.scott@gmail.com>
2016-01-29 23:59:53 -0500

    adds lookup to stream

--- spec/lib/streamer/stream_spec.rb
+++ spec/lib/streamer/stream_spec.rb
@@ -8,9 +8,21 @@ describe 'Stream' do
         { 'month' => 'jan', 'score' => 1 },
         { 'month' => 'feb', 'score' => 2 },
         { 'month' => 'mar', 'score' => 3 }
+      ],
+      'sales' => [
+        { 'product' => 'product1', 'units' => 100, 'amount' => 280 },
+        { 'product' => 'product2', 'units' => 300, 'amount' => 2800 },
+        { 'product' => 'product3', 'units' => 200, 'amount' => 1700 }
       ]
     }
     @stream = @subject.new(@hash)
+    @facts = {
+      'products' => {
+        'product1' => { 'rate' => 0.1, 'group' => 'category 3' },
+        'product2' => { 'rate' => 0.2, 'group' => 'category 1' },
+        'product3' => { 'rate' => 0.3, 'group' => 'category 2' }
+      }
+    }
   end
 
   describe 'assign' do
@@ -23,12 +35,12 @@ describe 'Stream' do
 
     it 'assigns the value to the property in the payload' do
       @stream.assign(property: 'assign_value', value: 5)
-        .payload['assign_value'].must_equal 5
+             .payload['assign_value'].must_equal 5
     end
 
     it 'can assign deeply nested properties' do
       @stream.assign(property: 'deeply.nested.property', value: 'w00t!')
-        .payload['deeply']['nested']['property'].must_equal 'w00t!'
+             .payload['deeply']['nested']['property'].must_equal 'w00t!'
     end
 
     it 'assigns the function value to the property in the payload' do
@@ -71,6 +83,30 @@ describe 'Stream' do
       @stream.payload['scores'][0]['rate'].must_be_within_delta 0.1, 0.00001
       @stream.payload['scores'][1]['rate'].must_be_within_delta 0.1, 0.00001
       @stream.payload['scores'][2]['rate'].must_be_within_delta 0.1, 0.00001
+    end
+  end
+
+  describe 'fact_finder' do
+    before :each do
+      @stream.finder = Streamer::Finder.new(
+        Streamer::FactProviders::HashProvider.new(@facts)
+      )
+    end
+
+    it 'can lookup values' do
+      @stream.assign_each(
+        list: 'sales',
+        property: 'rate',
+        function: {
+          type: 'lookup',
+          fact: 'products.#rate',
+          terms: ['#product'],
+          finder: @stream.finder
+        }
+      )
+      @stream.payload['sales'][0]['rate'].must_be_within_delta 0.1, 0.00001
+      @stream.payload['sales'][1]['rate'].must_be_within_delta 0.2, 0.00001
+      @stream.payload['sales'][2]['rate'].must_be_within_delta 0.3, 0.00001
     end
   end
 end
