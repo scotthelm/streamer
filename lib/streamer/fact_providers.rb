3d48ebf21ef5e80322727265f18a78c051edd4e8
Scott Helm <helm.scott@gmail.com>
2016-01-31 19:00:56 -0500

    adds interface for fact providers

--- lib/streamer/fact_providers.rb
+++ lib/streamer/fact_providers.rb
@@ -4,5 +4,10 @@ require 'streamer/fact_providers/hash_provider'
 module Streamer
   # FactProviders are used by finders to find facts
   module FactProviders
+    # Interface for FactProviders : for use by other libs
+    module Interface
+      def find(key)
+      end
+    end
   end
 end
