91d054dba15267c61d8ae70a2f5906dbc46d8b50
Scott Helm <helm.scott@gmail.com>
2016-02-01 22:02:21 -0500

    implements streambuilder filter

--- lib/streamer/stream_builder.rb
+++ lib/streamer/stream_builder.rb
@@ -2,16 +2,23 @@ module Streamer
   # StreamBuilder takes a stream configuration and builds the stream defined
   # in it. StreamBuilder then performs the stream, given a document.
   class StreamBuilder
-    attr_reader :config, :payload, :stream
+    attr_reader :config, :payload
     def initialize(config)
       @config = config
     end
 
     def flow(payload)
       @payload = payload
-      @stream = Stream.new(payload)
-      @stream.filter(function: config[:filter].first[:function])
-      @stream
+      filter_value = config[:filter].inject(true) do |val, f|
+        val &&= stream.filter(function: f[:function]).payload['filter_value']
+        val
+      end
+      stream.payload['filter_value'] = filter_value
+      stream
+    end
+
+    def stream
+      @stream ||= Stream.new(payload)
     end
   end
 end
