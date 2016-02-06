f55a62bba8cc706baae873942c1ae0b20ab96e21
Scott Helm <helm.scott@gmail.com>
2016-02-05 21:16:34 -0500

    attempt test coverage

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
 
 gem 'pry'
-
+gem 'codeclimate-test-reporter', group: :test, require: nil
 gemspec
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 $LOAD_PATH.unshift File.expand_path('../support', __FILE__)
+
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
 require 'streamer'
 
 require 'minitest/autorun'
