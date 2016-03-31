b08b114e2ebf49f0162801b114fbe1f55256d419
Scott Helm <helm.scott@gmail.com>
2016-03-31 14:12:15 -0400

    adds CSV fact provider with a naive awk lookup implementation
    adds quoted identifier FS and CSV line parsing
    
    updates version
    
    updates Gemfile for testing

---
+++ lib/streamer/fact_providers/csv_provider.rb
@@ -0,0 +1,52 @@
+require 'csv'
+
+module Streamer
+  module FactProviders
+    # CSVProvider implements Finder Provider interface
+    class CSVProvider
+      attr_reader :path, :key
+
+      def initialize(path)
+        @path = path
+      end
+
+      def find(key)
+        @key = key
+        return hash_result if segments.size == 2
+        return scalar_result if segments.size == 3
+      end
+
+      def segments
+        key.split('.')
+      end
+
+      def hash_result
+        Hash[headers.zip CSV.parse_line(lines)]
+      end
+
+      def scalar_result
+        hash_result[segments[2]]
+      end
+
+      def lines
+        `#{processing_statement}`.strip
+      end
+
+      def headers
+        @headers ||= CSV.parse_line(`head -n 1 #{path}`)
+      end
+
+      def field_number(field)
+        headers.index(field) + 1
+      end
+
+      def processing_statement
+        segs = segments
+        return unless segs.size > 1 && segs.size < 4
+        <<-STMT
+awk -F'","|,' '($#{field_number(segs[0])} ~ /^#{segs[1]}$/){print $0}' #{path}
+        STMT
+      end
+    end
+  end
+end
